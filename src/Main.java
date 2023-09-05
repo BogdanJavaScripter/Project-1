c2e933fa2391537b4ad8784f4328367e48743878
Колток Оксана Миколаївна <koltok-om@tsoua.com>
2023-09-05 20:26:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void  main(String[] args){
+        System.out.println("Hello everyone");
+        System.out.println("My mane is Bogdan Welcome to my first java program ");
+    }
+}
+
+
+
+
+
+
+
+
